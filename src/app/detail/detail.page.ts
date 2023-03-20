cd29cc9adf141668104cdd74c412df9dbb8cc0c8
Killian <killianchaoui511@gmail.com>
2023-03-20 15:35:24 +0100

    afficher liste initiation

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -10,16 +10,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DetailPage implements OnInit{
 
   lsthackathons:any;
+  lstevents:any
 
   constructor(private router: Router, private http : HttpClient, private activeRoute: ActivatedRoute) {
-this.activeRoute.queryParams.subscribe(params =>{
-  let item:any;
-  item=this.router.getCurrentNavigation()?.extras.state;
-  console.log(item.param1);
-  this.lsthackathons=item.param1;
-  
-})
+  this.activeRoute.queryParams.subscribe(params =>{
+    let item:any;
+    item=this.router.getCurrentNavigation()?.extras.state;
+    console.log(item.param1.id);
+    this.lsthackathons=item.param1;
 
+    this.http.get('https://127.0.0.1:8000/api/hackathon/'+ item.param2.id + '/evenements').subscribe(data => {
+      this.lstevents=data;
+      console.log(this.lstevents);
+    });
+  })
 }
 ngOnInit(){
 
