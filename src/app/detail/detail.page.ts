e1e03322fe34af1e5b10a3d14234564134cfb77d
Killian <killianchaoui511@gmail.com>
2023-05-05 08:54:06 +0200

    ajout favoris

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -7,25 +7,37 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
 })
-export class DetailPage implements OnInit{
+export class DetailPage implements OnInit {
+  lsthackathons: any;
+  lstevents: any;
 
-  lsthackathons:any;
-  lstevents:any
+  constructor(private router: Router, private http: HttpClient, private activeRoute: ActivatedRoute) {}
 
-  constructor(private router: Router, private http : HttpClient, private activeRoute: ActivatedRoute) {
-  this.activeRoute.queryParams.subscribe(params =>{
-    let item:any;
-    item=this.router.getCurrentNavigation()?.extras.state;
-    console.log(item.param1.id);
-    this.lsthackathons=item.param1;
+  ngOnInit() {
+    this.activeRoute.queryParams.subscribe(params => {
+      let item: any;
+      item = this.router.getCurrentNavigation()?.extras.state;
+      console.log(item.param1.id);
+      this.lsthackathons = item.param1;
 
-    this.http.get('https://127.0.0.1:8000/api/hackathon/'+ item.param1.id + '/evenements').subscribe(data => {
-      this.lstevents=data;
-      console.log(this.lstevents);
+      const hackathonId = this.lsthackathons.id; // Variable temporaire
+
+      this.http
+        .get('https://127.0.0.1:8000/api/hackathon/' + hackathonId + '/evenements')
+        .subscribe(data => {
+          this.lstevents = data;
+          console.log(this.lstevents);
+        });
     });
-  })
-}
-ngOnInit(){
+  }
 
-}
+  addToFavorites() {
+    let favorites: any[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+    const isFavorite = favorites.some((fav: any) => fav.id === this.lsthackathons.id);
+
+    if (!isFavorite) {
+      favorites.push(this.lsthackathons);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  favorites: any[] = [];
 
   constructor() {}
 
+  ngOnInit() {
+    this.getFavorites();
+  }
+
+  getFavorites() {
+    const favoritesData = localStorage.getItem('favorites');
+    if (favoritesData) {
+      this.favorites = JSON.parse(favoritesData);
+    }
+  }
+
+  removeFavorite(favorite: any) {
+    const index = this.favorites.findIndex((fav: any) => fav.id === favorite.id);
+    if (index > -1) {
+      this.favorites.splice(index, 1);
+      localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    }
+  }
 }
